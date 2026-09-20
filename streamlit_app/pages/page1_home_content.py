4b9fa540582e4d7c5dfe3658e022e38d89cbe0a6
ptilrem <paltilrem@gmail.com>
2026-09-20 20:58:22 +0200

    Added a light theme (and ability to swap between light and dark mode)

--- streamlit_app/pages/page1_home_content.py
+++ streamlit_app/pages/page1_home_content.py
@@ -5,4 +5,5 @@ import streamlit as st
 st.title("🏠 Home Page")
 st.write("Welcome to the Home Page!"
          " Use the sidebar to navigate between the pages."
+         " Switch between dark and light mode using the three dots in the top right corner"
          )
