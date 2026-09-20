e7b788e1a179d372aec091fd3a30038bfdbe8630
ptilrem <paltilrem@gmail.com>
2026-09-20 21:01:43 +0200

    Changed text layout page 1

--- streamlit_app/pages/page1_home_content.py
+++ streamlit_app/pages/page1_home_content.py
@@ -3,7 +3,6 @@ import streamlit as st
 
 # Set title and page text
 st.title("🏠 Home Page")
-st.write("Welcome to the Home Page!"
-         " Use the sidebar to navigate between the pages."
-         " Switch between dark and light mode using the three dots in the top right corner"
-         )
+st.write("Welcome to the Home Page!")
+st.write("Use the sidebar to navigate between the pages.")
+st.write("Switch between dark and light mode using the three dots in the top right corner")
